47fbf102373274454f60b54fb6d35e95f9c95b89
John L. Preston <wcerfgba@riseup.net>
2017-07-28 20:52:01 +0100

    Split context from examples.

--- spec/surhash_spec.rb
+++ spec/surhash_spec.rb
@@ -1,12 +1,26 @@
 require './lib/surhash'
 
-# one day...
-# require 'faker'
 
-RSpec.describe SurHash do
-  describe '.[]' do
-    it 'constructs a surhash from a complex literal' do
-      actual = SurHash[
+# Contexts
+
+RSpec.shared_context 'empty' do
+  subject do
+    SurHash.empty
+  end
+end
+RSpec.shared_context 'one-to-one' do
+  subject do
+    SurHash[ 'A'  =>  1 ]
+  end
+end
+RSpec.shared_context 'many-to-one' do
+  subject do
+    SurHash[  [ 'A', 'B', 'C' ] =>  1 ]
+  end
+end
+RSpec.shared_context 'complex' do
+  subject do
+    SurHash[
         'A'               =>  1,
         'B'               =>  2,
         [ 'C', 'D' ]      =>  3,
@@ -15,14 +29,24 @@ RSpec.describe SurHash do
         [ 'I', 'J' ]      =>  6,
         'K'               =>  7,
       ]
+  end
+end
 
-      actual.should_not be_empty
-      actual.size.should eq 11
-      actual.keys.size.should eq 11
-      actual.values.size.should eq 7
-      actual.keys.should eq 'A'..'K'
-      actual.values.should eq 1..7
-      actual.entries.should eq [
+
+# Examples
+
+RSpec.describe SurHash do
+  describe '.[]' do
+    include_context 'complex'
+    
+    it 'constructs a surhash from a complex literal' do
+      subject.should_not be_empty
+      subject.size.should eq 11
+      subject.keys.size.should eq 11
+      subject.values.size.should eq 7
+      subject.keys.should eq 'A'..'K'
+      subject.values.should eq 1..7
+      subject.entries.should eq [
         [ 'A', 1 ],
         [ 'B', 2 ],
         [ 'C', 3 ],
@@ -38,18 +62,37 @@ RSpec.describe SurHash do
     end
   end
   describe '.empty' do
-    it 'returns an empty surhash' do
-      actual = SurHash.empty
-      
-      actual.should be_empty
-      actual.keys.should be_empty
-      actual.values.should be_empty
+    include_context 'empty'
+
+    it 'returns an empty surhash' do    
+      subject.should be_empty
+      subject.keys.should be_empty
+      subject.values.should be_empty
     end
   end
   describe '.empty?' do
+    context 'empty' do
+      include_context 'empty'
+
+      it 'return true if the surhash is empty' do
+        subject.empty?.should be true
+      end
+    end
+    context 'not empty'
+      include_context 'many-to-one'
+
+      it 'returns false if the surhash contains elements' do
+        subject.empty?.should be false
+      end
+    end
   end
   describe '#size' do
-  end
+    context 'empty' do
+      include_context 'empty'
+      it 'should return 0 if the surhash is empty' do
+        subject.size.should eq 0
+      end
+    end
   describe '#keys' do
   end
   describe '#values' do
